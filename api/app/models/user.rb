9f6c30972486e622367236102785b334022d045d
shuaib shaban <shuaib>
2023-03-23 05:45:09 +0300

    implemented sign up

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
-    has_secure_password
+   has_secure_password
 
-    validates :username, { 
-        length: { minimum: 5, maximum: 8 }, 
-        uniqueness: true,
-        presence: true
+   validates :username, { 
+       length: { minimum: 5, maximum: 8 }, 
+       uniqueness: true,
+       presence: true
 
-     }
+    }
 
-     validates :email, {
-        uniqueness: true,
-        presence: true
-     }
+    validates :email, {
+       uniqueness: true,
+       presence: true
+    }
 end
